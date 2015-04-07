26c9a3a0e5b26cb3c258cbaf8bafc6bf1727c26b
Teresa Lamb <flour1337@gmail.com>
2015-04-07 00:42:11 -0400

    pattern form added

--- routes/index.js
+++ routes/index.js
@@ -11,51 +11,97 @@ router.get('/helloworld', function(req, res, next) {
   res.render('helloworld', { title: 'Hello,World!' });
 });
 
+/* GET Patternlist page. */
+router.get('/patternlist', function(req, res, next) {
+    //get db object
+    var db = req.db;
+    //tell app which collection to look at
+    var collection = db.get('patterncollection');
+    //do a find in this collection
+    //return results as the variable 'docs'
+    collection.find({},{},function(e,docs) {
+        res.render('patternlist', {
+            "patternlist" : docs
+        });
+    });
+});
+
+/* GET New Pattern page. */
+router.get('/newpattern', function(req,res) {
+    res.render('newpattern', { title: 'Add New Pattern' });
+});
+
+/*POST to Add Pattern Service */
+router.post('/addpattern', function(req,res) {
+    //get db object
+    var db = req.db;
+    //get form values
+    //these rely on "name" attribute 
+    var patternName = req.body.name;
+    var patternType = req.body.type;
+    //tell app which collection to look at
+    var collection = db.get('patterncollection');
+    //add to the db
+    collection.insert({
+        "name" : patternName,
+        "type" : patternType
+    }, function (err, doc) {
+        if (err) {
+            //if it failed, return error
+            res.send("There was a problem adding the info to the database.");
+        }
+        else {
+            //if it worked, set the header set the address bar
+            res.location("patternlist");
+            //forward to success page
+            res.redirect("patternlist");
+        }
+    });
+});
+
 /* GET Userlist page. */
 router.get('/userlist', function(req, res, next) {
-	//get db object
-	var db = req.db;
-	//tell app which collection to look at
-	var collection = db.get('usercollection');
-	//do a find in this collection
-	//return results as the variable 'docs'
-	collection.find({},{},function(e,docs){
-		res.render('userlist', {
-			"userlist" : docs
-		});
-	});
+    //get db object
+    var db = req.db;
+    //tell app which collection to look at
+    var collection = db.get('usercollection');
+    //do a find in this collection
+    //return results as the variable 'docs'
+    collection.find({},{},function(e,docs) {
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
 });
 
 /* GET New User page. */
 router.get('/newuser', function(req,res) {
-	res.render('newuser', { title: 'Add New User' });
+    res.render('newuser', { title: 'Add New User' });
 });
-/* POST to Add User Service */
-router.post('/adduser', function(req, res) {
 
-    // Set our internal DB variable
+/*POST to Add User Service */
+router.post('/adduser', function(req,res) {
+    //get db object
     var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
+    //get form values
+    //these rely on "name" attribute 
     var userName = req.body.username;
     var userEmail = req.body.useremail;
-
-    // Set our collection
+    //tell app which collection to look at
     var collection = db.get('usercollection');
-
-    // Submit to the DB
+    //add to the db
     collection.insert({
         "username" : userName,
         "email" : userEmail
     }, function (err, doc) {
         if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
+            //if it failed, return error
+            res.send("There was a problem adding the info to the database.");
         }
         else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
+            //if it worked, set the header set the address bar
             res.location("userlist");
-            // And forward to success page
+            //forward to success page
             res.redirect("userlist");
         }
     });
