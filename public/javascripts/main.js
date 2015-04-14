ac2f6df34282e17fbf95587eb2194e33b71aca5d
Teresa Lamb <flour1337@gmail.com>
2015-04-14 19:48:29 -0400

    adding show and hide to multiple elements

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,13 +3,26 @@ function init() {
 	console.log("hello");
 
 	var expand = document.querySelector('.expand');
-	var plus = document.querySelector('.plus');
-	plus.addEventListener('click', show);
 	
-	var minus = document.querySelector('.minus');
-	minus.addEventListener('click', hide);
+	var plus = document.querySelector('.plus');
+		plus.addEventListener('click', show);
 
-	function show(){	
+	var minus = document.querySelector('.minus');
+		minus.addEventListener('click', hide);
+
+	// var plus = document.querySelectorAll('.plus');
+	// for (var i = 0; i < plus.length; i++) {
+	// 	plus[i].addEventListener('click', show);
+	// 	console.log(i);
+	// };
+	
+	// var minus = document.querySelectorAll('.minus');
+	// for (var i = 0; i < minus.length; i++) {
+	// 	minus[i].addEventListener('click', hide);
+	// 	console.log(i);
+	// };
+
+	function show(){
 		plus.classList.add('hidden');
 		minus.classList.remove('hidden');
 		expand.classList.remove('hidden');
