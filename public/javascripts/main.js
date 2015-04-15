a5b285e6f0e6e98eb7f43eb11e02cc0ce3309f91
Teresa Lamb <flour1337@gmail.com>
2015-04-14 20:23:03 -0400

    plus and minus showing and hiding

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,34 +4,35 @@ function init() {
 
 	var expand = document.querySelector('.expand');
 	
-	var plus = document.querySelector('.plus');
-		plus.addEventListener('click', show);
+	// var plus = document.querySelector('.plus');
+	// 	plus.addEventListener('click', show);
 
-	var minus = document.querySelector('.minus');
-		minus.addEventListener('click', hide);
+	// var minus = document.querySelector('.minus');
+	// 	minus.addEventListener('click', hide);
 
-	// var plus = document.querySelectorAll('.plus');
-	// for (var i = 0; i < plus.length; i++) {
-	// 	plus[i].addEventListener('click', show);
-	// 	console.log(i);
-	// };
+	var plus = document.querySelectorAll('.plus');
+	for (var i = 0; i < plus.length; i++) {
+		plus[i].addEventListener('click', show);
+		console.log(i);
+	};
 	
-	// var minus = document.querySelectorAll('.minus');
-	// for (var i = 0; i < minus.length; i++) {
-	// 	minus[i].addEventListener('click', hide);
-	// 	console.log(i);
-	// };
+	var minus = document.querySelectorAll('.minus');
+	for (var i = 0; i < minus.length; i++) {
+		minus[i].addEventListener('click', hide);
+		console.log(i);
+	};
 
-	function show(){
-		plus.classList.add('hidden');
-		minus.classList.remove('hidden');
-		expand.classList.remove('hidden');
+	function show(e){
+		console.log(e.target.parentElement.nextElementSibling);
+		e.target.parentElement.classList.add('hidden');
+		e.target.parentElement.nextElementSibling.classList.remove('hidden');
+		//expand.classList.remove('hidden');
 	}
 
-	function hide(){
-		plus.classList.remove('hidden');
-		minus.classList.add('hidden');
-		expand.classList.add('hidden');
+	function hide(e){
+		e.target.parentElement.previousElementSibling.classList.remove('hidden');
+		e.target.parentElement.classList.add('hidden');
+		//expand.classList.add('hidden');
 	}
 
 };
