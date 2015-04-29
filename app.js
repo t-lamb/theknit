d1cadd92f434a1b8847b219f1b04b8cb1081118f
t-lamb <flour1337@gmail.com>
2015-04-29 19:12:41 -0400

    removed /users route

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ db.on('open', function(){
 
 //connect to index.js
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -49,7 +48,6 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
